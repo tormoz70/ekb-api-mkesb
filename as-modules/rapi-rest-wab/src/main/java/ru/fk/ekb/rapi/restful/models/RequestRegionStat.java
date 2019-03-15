8943618977845c7c006fa73f48b11e3c2f8554eb
mishak <kondratyev@logistix.ru>
2019-03-15 18:17:25 +0300

    POST

---
+++ as-modules/rapi-rest-wab/src/main/java/ru/fk/ekb/rapi/restful/models/RequestRegionStat.java
@@ -0,0 +1,8 @@
+package ru.fk.ekb.rapi.restful.models;
+
+public class RequestRegionStat {
+    public String region;
+    public String cardNumbers;
+    public String startDate;
+    public String endDate;
+}
--- as-modules/rapi-rest-wab/src/main/java/ru/fk/ekb/rapi/restful/srvc/api/APISrvc.java
+++ as-modules/rapi-rest-wab/src/main/java/ru/fk/ekb/rapi/restful/srvc/api/APISrvc.java
@@ -1,6 +1,8 @@
 package ru.fk.ekb.rapi.restful.srvc.api;
 
 import ru.bio4j.ng.commons.converter.Converter;
+import ru.bio4j.ng.commons.utils.Jsons;
+import ru.bio4j.ng.commons.utils.Strings;
 import ru.bio4j.ng.commons.utils.Utl;
 import ru.bio4j.ng.model.transport.ABean;
 import ru.bio4j.ng.model.transport.ABeanPage;
@@ -11,8 +13,14 @@ import javax.servlet.http.HttpServletRequest;
 import javax.ws.rs.*;
 import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
-import java.util.ArrayList;
+import java.io.BufferedReader;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.lang.reflect.Array;
 import java.util.List;
+
+import ru.bio4j.ng.service.types.BioQueryParams;
+import ru.bio4j.ng.service.types.BioWrappedRequest;
 
 @Path("/api")
 public class APISrvc extends RestSrvcBase {
@@ -20,8 +28,40 @@ public class APISrvc extends RestSrvcBase {
     @GET
     @Path("/region-stat")
     @Produces(MediaType.APPLICATION_JSON)
-    public List<PuQty> region_stat(@Context HttpServletRequest request) throws Exception {
+    public List<PuQty> region_stat_get(@Context HttpServletRequest request) throws Exception {
         List<PuQty> puQties = _getList("api.region_stat", request, PuQty.class);
         return puQties;
+    }
+
+    public void ReadStreamToStr(InputStream inputStream, String encoding, String json) throws Exception {
+        StringBuffer stringBuffer = new StringBuffer();
+        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream, encoding));
+        String buffer = null;
+        while ((buffer = bufferedReader.readLine()) != null){
+            stringBuffer.append(buffer);
+        }
+        json = stringBuffer.toString();
+    }
+
+    @POST
+    @Path("/region-stat")
+    @Produces(MediaType.APPLICATION_JSON)
+    public  List<PuQty> region_stat_post(@Context HttpServletRequest request) throws Exception {
+        RequestRegionStat requestRegionStat = null;
+        BioQueryParams queryParams = ((BioWrappedRequest)request).getBioQueryParams();
+        if(!Strings.isNullOrEmpty(queryParams.jsonData)) {
+            try {
+                requestRegionStat = Jsons.decode(queryParams.jsonData, RequestRegionStat.class);
+            } catch (Exception e) {
+//                LOG.error(String.format("Ошибка при получении Json-параметров запроса: %s", queryParams.jsonData), e);
+            }
+            _setBioParamToRequest("region", requestRegionStat.region, request);
+            _setBioParamToRequest("cardNumbers", requestRegionStat.cardNumbers, request);
+            _setBioParamToRequest("startDate", requestRegionStat.startDate, request);
+            _setBioParamToRequest("endDate", requestRegionStat.endDate, request);
+            List<PuQty> puQties = _getList("api.region_stat", request, PuQty.class, true);
+            return  puQties;
+        }
+        return null;
     }
 }
