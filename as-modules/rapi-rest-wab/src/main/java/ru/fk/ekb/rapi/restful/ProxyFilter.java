416d7b78974006d17f9d0c00ae4eeb1be1e114f8
bio <gpsdom@gmail.com>
2019-05-07 16:27:32 +0300

    wc

--- as-modules/rapi-rest-wab/src/main/java/ru/fk/ekb/rapi/restful/ProxyFilter.java
+++ as-modules/rapi-rest-wab/src/main/java/ru/fk/ekb/rapi/restful/ProxyFilter.java
@@ -2,12 +2,9 @@ package ru.fk.ekb.rapi.restful;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
-import ru.bio4j.ng.service.types.WrappedRequest;
 import ru.bio4j.ng.service.types.WarSecurityFilterBase;
 
 import javax.servlet.*;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 public class ProxyFilter extends WarSecurityFilterBase implements Filter {
@@ -20,27 +17,7 @@ public class ProxyFilter extends WarSecurityFilterBase implements Filter {
 
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
-        //chain.doFilter(request, response);
-        //super.doFilter(request, response, chain);
-
-        final HttpServletRequest req = (HttpServletRequest) request;
-        try {
-            WrappedRequest rereq = new WrappedRequest(req);
-            rereq.putHeader("Access-Control-Allow-Origin", "*");
-            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
-            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Credentials", "true");
-            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, X-SToken, Authorization");
-            super.doFilter(rereq, response, chain);
-        } catch (Exception ex) {
-//            throw new ServletException(ex);
-            LOG.error(null, ex);
-            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Origin", "*");
-            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Credentials", "true");
-            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE, PATCH");
-            ((HttpServletResponse)response).setHeader("Access-Control-Allow-Headers", "Access-Control-Allow-Credentials, Origin, X-Requested-With, Content-Type, Accept, X-SToken, X-Pagination-Current-Page, X-Pagination-Per-Page, Authorization");
-            ((HttpServletResponse)response).sendError(HttpServletResponse.SC_INTERNAL_SERVER_ERROR);
-        }
+        super.doFilter(request, response, chain);
     }
 
     @Override
